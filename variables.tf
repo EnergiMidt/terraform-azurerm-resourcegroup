b2a277524d51fb1f94d2af6a864729b0cf6093a6
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-04 17:15:41 +0200

    fix: typographical error

--- variables.tf
+++ variables.tf
@@ -12,23 +12,23 @@ variable "environment" {
 }
 
 variable "location" {
-  description = "(Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+  description = "(Required) The location where the resource should exist. Changing this forces a new resource to be created."
   type        = string
 }
 
 variable "name" {
-  description = "(Required) The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
+  description = "(Required) The name which should be used for the resource. Changing this forces a new resource to be created."
   type        = string
 }
 
 variable "override_name" {
   description = "(Optional) Override the name of the resource. Under normal circumstances, it should not be used."
-  default     = null
   type        = string
+  default     = null
 }
 
 variable "tags" {
   description = "(Optional) A mapping of tags to assign to the resource."
-  default     = {}
   type        = map(string)
+  default     = {}
 }
